6b56085e965ea6b28879ca99a7d98587ddc7b56b
cheungth25 <cheungth25@gmail.com>
2017-07-11 09:09:19 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+  validate :click_bait?
+
+
+  def click_bait?
+    click_bait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    click_bait_check = click_bait.map { |test| title =~ test}
+    errors.add(:title, "looks like clickbait") if !(click_bait_check.compact.size > 0)
+  end
 end
